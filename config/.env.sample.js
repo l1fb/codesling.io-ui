5ae8de394c04a7c50fcc1cf00c954d3a4f1c5d57
l1fb <yon_gospel@hotmail.com>
2018-01-20 11:43:00 -0800

    [fix] fixes .env.sample.js to cater to dns

--- config/.env.sample.js
+++ config/.env.sample.js
@@ -6,16 +6,11 @@ const envBuild = {
     'NODE_ENV=DEVELOPMENT',
     'DEBUG=TRUE',
     'ENVPREFIX=REACT_APP_',
-    'REST_SERVER_URL=http://ec2-13-56-228-82.us-west-1.compute.amazonaws.com
-    :4990',
-    'SOCKET_SERVER_URL=http://ec2-13-56-228-82.us-west-1.compute.amazonaws.com
-    :4155',
-    'CODERUNNER_SERVICE_URL=http://ec2-13-56-228-82.us-west-1.compute.amazonaws.com
-    :4000',
-    'REACT_APP_SOCKET_SERVER_URL=http://ec2-13-56-228-82.us-west-1.compute.amazonaws.com
-    :4155',
-    'REACT_APP_REST_SERVER_URL=http://ec2-13-56-228-82.us-west-1.compute.amazonaws.com
-    :4990'
+    'REST_SERVER_URL=http://ec2-13-56-228-82.us-west-1.compute.amazonaws.com:4990',
+    'SOCKET_SERVER_URL=http://ec2-13-56-228-82.us-west-1.compute.amazonaws.com:4155',
+    'CODERUNNER_SERVICE_URL=http://ec2-13-56-228-82.us-west-1.compute.amazonaws.com:4000',
+    'REACT_APP_SOCKET_SERVER_URL=http://ec2-13-56-228-82.us-west-1.compute.amazonaws.com:4155',
+    'REACT_APP_REST_SERVER_URL=http://ec2-13-56-228-82.us-west-1.compute.amazonaws.com:4990'
   ]
 };
 
